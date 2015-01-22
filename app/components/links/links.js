ac521b0ed8666fb49984a6ad925a0987e6292100
Stoica Vlad <vlad.stoick@yahoo.co.uk>
2015-01-22 19:07:09 +0200

    initial work on #39

--- app/components/links/links.js
+++ app/components/links/links.js
@@ -12,6 +12,33 @@ angular.module('linksgrabberApp')
   	if(UserInfo.isAuthenticated === false){
   		$location.path('/login');
   	}
-	$scope.links = Links;
     $scope.shouldshowallmedia = false;
+    $scope.links = [];
+	$scope.isBusy = false;
+	$scope.isDone = false;
+	var page = 1;
+	var totalPages = -1;
+	
+	$scope.loadNextPage = function(){
+		if($scope.isDone === true){
+			return;
+		}
+		$scope.isBusy = true;
+		Links.getReceivedLinksPage(page)
+			.success(function(links){
+				totalPages = links.paging.total_pages;
+        		links = links.links;
+        		for(var i = 0; i < links.length ; i++){
+          			$scope.links.push(links[i]);
+        		}
+        		$scope.isBusy = false;
+        		page++;
+        		if(totalPages <= page){
+        			$scope.isDone = true;
+        		}
+			})
+			.error(function(error){
+				console.log(error);
+			});	
+	};
   });
--- app/components/services/links.js
+++ app/components/services/links.js
@@ -9,48 +9,26 @@
  */
 angular.module('linksgrabberApp')
   .factory('Links', function ($http, UserInfo, apiURL, $rootScope) { 
-    var Links = {
-      items : [],
-      isBusy : false,
-      isDone : false,
-      page : 1,
-      totalPages : -1
-    };
-
-    $rootScope.$on('logout',function(){
-      Links.items = [];
-      Links.isBusy = false;
-      Links.isDone = false;
-      Links.page = 1;
-      Links.totalPages = -1;
-    });
-
-    Links.nextPage = function(){
-      if(Links.page > Links.totalPages && Links.totalPages!==-1){
-        Links.isDone = true;
-        return;
-      }
-      Links.isBusy = true;
-      $http({
-        method : 'GET',
-        url : apiURL + '/users/me/links',
-        params : {
-          auth_token : UserInfo.apiToken,
-          page : Links.page
-        }
-      }).success(function(data) {
-        Links.totalPages = data.paging.total_pages;
-        data = data.links.map(function(link){
-          link.sender.facebookImgUrl = 'https://graph.facebook.com/v2.2/' +
-          link.sender.facebook_id + '/picture?width=100&height=100';    
-          return link;
+    return {
+      getReceivedLinksPage : function(page){
+        return $http({
+          method : 'GET',
+          url : apiURL + '/users/me/links/received',
+          params : {
+            auth_token : UserInfo.apiToken,
+            page : page
+          }
         });
-        for(var i = 0; i < data.length ; i++){
-          Links.items.push(data[i]);
-        }
-        Links.isBusy = false;
-      });
-      Links.page++;
+      },
+      getSentLinksPage : function(page){
+        return $http({
+          method : 'GET',
+          url : apiURL + '/users/me/links/sent',
+          params : {
+            auth_token : UserInfo.apiToken,
+            page : page
+          }
+        });
+      },
     };
-    return Links;
   });
