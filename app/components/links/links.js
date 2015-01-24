57afd31a90763372fb86530e19712389145b4078
Stoica Vlad <vlad.stoick@yahoo.co.uk>
2015-01-24 10:47:20 +0200

    implemented change button for #39

--- app/components/links/links.js
+++ app/components/links/links.js
@@ -12,33 +12,48 @@ angular.module('linksgrabberApp')
   	if(UserInfo.isAuthenticated === false){
   		$location.path('/login');
   	}
-    $scope.shouldshowallmedia = false;
-    $scope.links = [];
+
+  	$scope.shouldshowallmedia = false;
+  	$scope.typeOfLinks = 'received';
+  	$scope.links = [];
 	$scope.isBusy = false;
 	$scope.isDone = false;
 	var page = 1;
-	var totalPages = -1;
-	
+	var totalPages;
+
+	$scope.changeTypeOfLinks = function(type){
+		$scope.typeOfLinks = type;	
+		$scope.links = [];
+		$scope.isBusy = false;
+		$scope.isDone = false;		
+		page = 1;
+		$scope.loadNextPage();
+	}  
+    	
 	$scope.loadNextPage = function(){
 		if($scope.isDone === true){
 			return;
 		}
 		$scope.isBusy = true;
-		Links.getReceivedLinksPage(page)
-			.success(function(links){
-				totalPages = links.paging.total_pages;
-        		links = links.links;
-        		for(var i = 0; i < links.length ; i++){
-          			$scope.links.push(links[i]);
-        		}
-        		$scope.isBusy = false;
-        		page++;
-        		if(totalPages <= page){
-        			$scope.isDone = true;
-        		}
-			})
-			.error(function(error){
-				console.log(error);
-			});	
+		var request =  Links.getReceivedLinksPage(page);
+		if($scope.typeOfLinks == 'sent'){
+			request = Links.getSentLinksPage(page);
+		}
+		request.success(function(links){
+			totalPages = links.paging.total_pages;
+    		for(var i = 0; i < links.links.length ; i++){
+      			$scope.links.push(links.links[i]);
+    		}
+    		$scope.isBusy = false;
+    		page++;
+    		if(totalPages <= page){
+    			$scope.isDone = true;
+    		}
+		})
+		.error(function(error){
+			console.log(error);
+		});	
 	};
+
+
   });
