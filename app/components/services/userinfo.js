12ede0a7c2d1853036a5e3de05cb07c6e358ac34
Stoica Vlad <vlad.stoick@yahoo.co.uk>
2015-01-24 21:33:30 +0200

    more work for #40

--- app/components/services/userinfo.js
+++ app/components/services/userinfo.js
@@ -14,16 +14,16 @@ angular.module('linksgrabberApp')
     var user = {
       fullName : '',
       profilePic : '',
-      facebookToken : '',
-      apiToken: '',
+      provider : '',
+      apiToken : '',
+      authToken : '',
       firstLoad : true
     };
 
+    var facebookAuthToken = '';
+
     user.logout = function(){
       $auth.logout();
-      user.profilePic = '';
-      user.fullName = '';
-      user.authToken = '';
       user.isAuthenticated = false;
       $rootScope.$broadcast('logout');
     };
@@ -31,17 +31,23 @@ angular.module('linksgrabberApp')
     user.isAuthenticated = false;
 
     user.login = function (provider){
-      return $auth.authenticate(provider);
+      var promise =  $auth.authenticate(provider);
+      promise.then(function(){
+        localStorage.provider = provider;
+      });
+      return promise;
     };
 
     user.loadData = function(){
+      user.provider = localStorage.provider;
       user.authToken = $auth.getPayload().token;
       user.apiToken = $auth.getPayload().userAuth;
-      user.profilePic = 'https://graph.facebook.com/v2.2/me/picture?width=100&height=100&access_token='+user.authToken;   
-      $http.get('https://graph.facebook.com/v2.2/me?fields=name&access_token='+user.authToken).success(function(data){
-        user.fullName = data.name;
-        // $rootScope.$broadcast('userDataLoaded');
-      });
+      if(user.provider === 'facebook'){
+        user.profilePic = 'https://graph.facebook.com/v2.2/me/picture?width=100&height=100&access_token='+user.authToken;   
+        $http.get('https://graph.facebook.com/v2.2/me?fields=name&access_token='+user.authToken).success(function(data){
+          user.fullName = data.name;
+        });
+      }
       
     }
 
