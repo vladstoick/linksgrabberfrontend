33e97de4d124376f6d334bab8723ffcd302f54d5
Stoica Vlad <vlad.stoick@yahoo.co.uk>
2015-01-25 21:37:50 +0200

    build change

--- Gruntfile.js
+++ Gruntfile.js
@@ -376,11 +376,6 @@ module.exports = function (grunt) {
       ],
       test: [
         'compass'
-      ],
-      dist: [
-        'compass:dist',
-        'imagemin',
-        'svgmin'
       ]
     },
 
